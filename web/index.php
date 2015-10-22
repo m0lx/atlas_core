ae6fc6b72648e547696cee3a4d46e003ab7eb123
m0lx <maxime.murati@me.com>
2015-10-22 12:12:30 +0200

    Init project

---
+++ web/index.php
@@ -0,0 +1,42 @@
+<?php
+
+// web/index.php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$blogPosts = array(
+    1 => array(
+        'date'      => '2011-03-29',
+        'author'    => 'igorw',
+        'title'     => 'Using Silex',
+        'body'      => '...',
+    ),
+);
+
+$app = new Silex\Application();
+
+$app->get('/', function () {
+    return 'Hello world';
+});
+
+$app->get('/blog', function () use ($blogPosts) {
+    $output = '';
+    foreach ($blogPosts as $post) {
+        $output .= $post['title'];
+        $output .= '<br />';
+    }
+
+    return $output;
+});
+
+$app->get('/blog/{id}', function (Silex\Application $app, $id) use ($blogPosts) {
+    if (!isset($blogPosts[$id])) {
+        $app->abort(404, "Post $id does not exist.");
+    }
+
+    $post = $blogPosts[$id];
+
+    return  "<h1>{$post['title']}</h1>".
+            "<p>{$post['body']}</p>";
+});
+
+$app->run();
